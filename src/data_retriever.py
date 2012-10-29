53fc0f87da0d49dc964d9e8a43a9c244920625a9
wilmott <wilmott@wilmott-VirtualBox.(none)>
2012-10-29 17:51:12 -0400

    just before turing on multithreading for quote download

--- src/data_retriever.py
+++ src/data_retriever.py
@@ -1,37 +1,31 @@
 import os
 import urllib2
-
-mike = "mike!"
-
-class bla(object):
-
-    def __init__(self):
-
-        print "in init"
-        self.test = "hello"
-
-    def get(self):
-        print self.test
-
+import threading
+import logging
+import time
 
 def get_yahoo_data():
     #This procedure will work after the DOS line endings have already been converted to LINUX. Use: sed -i 's/\r//' filename
     stock_list = open('/home/wilmott/Desktop/fourseasons/fourseasons/data/large_universe', 'r')
     symbols = stock_list.read().split('\n')
     stock_list.close()
-
-    print symbols
-    print"length", len(symbols)
+    print"Number of symbols to fetch: ", len(symbols)
 
     for s in symbols:
         print s
         #single_quote_request = "curl --silent 'http://download.finance.yahoo.com/d/quotes.csv?s=" + s + "&f=l' > tmp/" + s + ".csv"
-        full_refresh_request = "curl 'http://table.finance.yahoo.com/table.csv?s=" + s + "&a=1&b=1&c=1950&d=10e=29&f=2012&ignore=.csv' > tmp/" + s + ".csv"
-        print full_refresh_request
+        file_path = "/home/wilmott/Desktop/fourseasons/fourseasons/tmp/" + s + ".csv"
+        
         try:
-            file_path = "/home/wilmott/Desktop/fourseasons/fourseasons/tmp/" + s + ".csv"
             if (os.path.exists(file_path) == False) or os.path.getsize(file_path) < 3500:
-                os.system(full_refresh_request)
+                post_request = "http://table.finance.yahoo.com/table.csv?s=" + s + "&a=1&b=1&c=1950&d=10e=29&f=2012&ignore=.csv"
+                print post_request, "\n\n"
+                test_file = urllib2.urlopen(post_request).read()
+                file_path = "/home/wilmott/Desktop/fourseasons/fourseasons/tmp/" + s + ".csv"
+
+                fout = open("/home/wilmott/Desktop/fourseasons/fourseasons/tmp/" + s + ".csv", 'w')
+                fout.write(test_file)
+                fout.close()
             else:
                 continue
         except:
@@ -44,10 +38,49 @@ def get_yahoo_data():
 def yahoo_post():
     s= 'XOM'
     post_request = "http://table.finance.yahoo.com/table.csv?s=" + s + "&a=1&b=1&c=1950&d=10e=29&f=2012&ignore=.csv"
-    
     test_file = urllib2.urlopen("http://table.finance.yahoo.com/table.csv?s=" + s + "&a=1&b=1&c=1950&d=10e=29&f=2012&ignore=.csv").read()
-    print test_file
-
-    fout = open("/home/wilmott/Desktop/fourseasons/fourseasons/tmp/" + "ZZZZZ" + ".csv", 'w')
-    #fout.write("This is a test")
+    fout = open("/home/wilmott/Desktop/fourseasons/fourseasons/tmp/" + s + ".csv", 'w')
     fout.write(test_file)
+
+def multithread_yahoo():
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(threadName)-10s) %(message)s',
+                        )
+
+    def daemon(s):
+        logging.debug('Starting ' + s)
+        time.sleep(2)
+        logging.debug('Exiting ' + s)
+
+    d = threading.Thread(name='daemon', target=daemon)
+    d.setDaemon(True)
+
+    t = threading.Thread(name='non-daemon', target=non_daemon)
+
+    stock_list = 'XOM\nMSFT\nAAPL\nGOOG\nJNJ\nPG\nF\nGM\nX\nAKS\nGLD\nSLV\nNEM\nABX'
+    symbols = stock_list.split('\n')
+
+    main_thread = threading.currentThread()
+    for s in symbols:
+
+        if len(threading.enumerate()) <= 3:
+            print "if ", s, len(threading.enumerate())
+            d = threading.Thread(name='daemon', target=daemon, args=[s])
+            d.setDaemon(True)
+            d.start()
+
+        else:
+            print "else ", s, len(threading.enumerate())
+            d.join()
+            d = threading.Thread(name='daemon', target=daemon, args=[s])
+            d.setDaemon(True)
+            d.start()
+
+    d.join()
+
+
+
+
+
+
--- test_run.py
+++ test_run.py
@@ -1,5 +1,45 @@
-from src.data_retriever import bla
 from src.data_retriever import *
 
 #get_yahoo_data()
-yahoo_post()
+#yahoo_post()
+multithread_yahoo()
+
+
+#import threading
+#import logging
+#import time
+#
+#logging.basicConfig(level=logging.DEBUG,
+#                    format='%(threadName)-10s) %(message)s',
+#                    )
+#
+#def daemon(s):
+#    logging.debug('Starting ' + s)
+#    time.sleep(2)
+#    logging.debug('Exiting ' + s)
+#
+#d = threading.Thread(name='daemon', target=daemon)
+#d.setDaemon(True)
+#
+#t = threading.Thread(name='non-daemon', target=non_daemon)
+#
+#stock_list = 'XOM\nMSFT\nAAPL\nGOOG\nJNJ\nPG\nF\nGM\nX\nAKS\nGLD\nSLV\nNEM\nABX'
+#symbols = stock_list.split('\n')
+#
+#main_thread = threading.currentThread()
+#for s in symbols:
+#
+#    if len(threading.enumerate()) <= 3:
+#        print "if ", s, len(threading.enumerate())
+#        d = threading.Thread(name='daemon', target=daemon, args=[s])
+#        d.setDaemon(True)
+#        d.start()
+#
+#    else:
+#        print "else ", s, len(threading.enumerate())
+#        d.join()
+#        d = threading.Thread(name='daemon', target=daemon, args=[s])
+#        d.setDaemon(True)
+#        d.start()
+#
+#d.join()
