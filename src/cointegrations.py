0b7553e0bf0de14cf1da6f510ebe2151171d8630
Michael Pilat <mcpilat@gmail.com>
2013-09-08 22:24:45 -0400

    ADF test is finally working correctly. Needs criteria to clean up.

--- src/cointegrations.py
+++ src/cointegrations.py
@@ -2,144 +2,18 @@ import numpy as np
 import scipy as scipy
 import datetime
 import time
-import copy
 
 from util.memoize import memoize, MemoizeMutable
 
 from data.redis import manage_redis
-from src import math_tools
-
-def get_corrected_data(stock_1_data, stock_2_data):
-	stock_1_trimmed, stock_2_trimmed = trim_data(stock_1_data, stock_2_data)
-
-	simple_1_close = np.empty(len(stock_1_trimmed))
-	simple_2_close = np.empty(len(stock_2_trimmed))
-
-	for k, v in enumerate(stock_1_trimmed):
-		simple_1_close[k] = stock_1_trimmed[k]['AdjClose']
-		simple_2_close[k] = stock_2_trimmed[k]['AdjClose']
-
-	simple_1_returns = math_tools.get_ln_returns(simple_1_close)
-	simple_2_returns = math_tools.get_ln_returns(simple_2_close)
-
-	return simple_1_close, simple_2_close, stock_1_trimmed, stock_2_trimmed
-	# return simple_1_returns, simple_2_returns
-
-
-def trim_data(stock_1_data, stock_2_data):
-
-	stock_1_start = datetime.datetime.strptime(stock_1_data[0]['Date'], '%Y-%m-%d').date()
-	stock_2_start = datetime.datetime.strptime(stock_2_data[0]['Date'], '%Y-%m-%d').date()
-
-	if stock_1_start < stock_2_start:
-		# print "1 is earlier"
-		for x in xrange(0, len(stock_1_data)):
-			# if datetime.datetime.strptime(stock_2_data[x]['Date'], '%Y-%m-%d').date() >= \
-			#    datetime.datetime.strptime(stock_1_data[0]['Date'], '%Y-%m-%d').date():
-			if stock_1_data[x]['Date'] == stock_2_data[0]['Date']:
-				trim_at = x
-				break
-		stock_1_data = stock_1_data[trim_at:]
-	
-	elif stock_2_start < stock_1_start:
-		# print "2 is earlier"
-		for x in xrange(0, len(stock_2_data)):
-			# if datetime.datetime.strptime(stock_2_data[x]['Date'], '%Y-%m-%d').date() >= \
-			#    datetime.datetime.strptime(stock_1_data[0]['Date'], '%Y-%m-%d').date():
-			if stock_2_data[x]['Date'] == stock_1_data[0]['Date']:
-				trim_at = x
-				break
-		stock_2_data = stock_2_data[trim_at:]
-
-	if len(stock_1_data) != len(stock_2_data):
-		stock_1_data, stock_2_data = propagate_on_fly(stock_1_data, stock_2_data)
-
-	if len(stock_1_data) != len(stock_2_data) or \
-		stock_2_data[len(stock_2_data)-1]['Date'] != stock_1_data[len(stock_1_data)-1]['Date'] or \
-		stock_2_data[0]['Date'] != stock_1_data[0]['Date']:
-		# print "\n**************"
-		# print stock_1_data[0]['Symbol'], stock_2_data[0]['Symbol']
-		# print len(stock_1_data), len(stock_2_data)
-		# print stock_1_data[0]['Date'], stock_2_data[0]['Date']
-		# print stock_1_data[len(stock_1_data)-1]['Date'], stock_2_data[len(stock_2_data)-1]['Date']
-		# for x in xrange(len(stock_1_data)):
-		# 		if stock_1_data[x]['Date'] != stock_2_data[x]['Date']:
-		# 			print stock_1_data[x]['Date'], stock_2_data[x]['Date']
-		# 			print "**************"
-		# 			break
-		e = stock_1_data[0]['Symbol'] + ' and ' + stock_2_data[0]['Symbol']
-		raise Exception(e + ' did not trim properly and cannot be processed')
-
-	# TODO: this section should be cythonized
-	for x in xrange(len(stock_1_data)):
-		if stock_1_data[x]['Date'] != stock_2_data[x]['Date']:
-			e = stock_1_data[0]['Symbol'] + ' and ' + stock_2_data[0]['Symbol']
-			print "\n\n"
-			raise Exception(e + ' did not trim properly and cannot be processed')
-
-	# if stock_1_end > stock_2_end:
-	# 	print "1 ends later"
-
-	# elif stock_2_end > stock_1_end:
-	# 	print "2 ends later"
-
-	return stock_1_data, stock_2_data
-
-def propagate_on_fly(stock_1_data, stock_2_data):
-
-	x_max = max(len(stock_1_data), len(stock_2_data))
-	
-	for x in xrange(0, x_max):
-		
-		# if x > 3397 and x < 3407:
-		# 	print x, stock_1_data[x]['Date'], stock_2_data[x]['Date'], stock_1_data[x]['Close'], stock_2_data[x]['Close']
-		if stock_1_data[x]['Date'] != stock_2_data[x]['Date']:
-			if datetime.datetime.strptime(stock_1_data[x]['Date'], '%Y-%m-%d').date() > \
-				datetime.datetime.strptime(stock_2_data[x]['Date'], '%Y-%m-%d').date():
-				temp = copy.deepcopy(stock_1_data[x-1])
-				temp['Date'] = copy.deepcopy(stock_2_data[x]['Date'])
-				stock_1_data.insert(x, temp)
-
-			elif datetime.datetime.strptime(stock_2_data[x]['Date'], '%Y-%m-%d').date() > \
-				datetime.datetime.strptime(stock_1_data[x]['Date'], '%Y-%m-%d').date():
-				temp = copy.deepcopy(stock_2_data[x-1])
-				temp['Date'] = copy.deepcopy(stock_1_data[x]['Date'])
-				stock_2_data.insert(x, temp)
-
-	# for x in xrange(3397, 3408):
-	# 	print x, stock_1_data[x]['Date'], stock_2_data[x]['Date'], stock_1_data[x]['Close'], stock_2_data[x]['Close']
-		# if stock_1_data[x]['Date'] != stock_2_data[x]['Date']:
-		# 	# print stock_1_data[x]['Date'], stock_2_data[x]['Date']
-		# 	break
-
-	return stock_1_data, stock_2_data
-
-
-def get_paired_stock_list(stocks, fixed_stock=None):
-
-	len_stocks = len(stocks)
-	paired_list = []
-	if fixed_stock is not None:
-		for x in xrange(0, len_stocks):
-			item = {'stock_1' : fixed_stock, 'stock_2' : stocks[x]}
-			paired_list.append(item)
-		print "Paired list length: ", len(paired_list)
-		return paired_list
-
-	for x in xrange(0, len_stocks):
-		stock_1 = stocks[x]
-
-		for y in xrange(x+1, len_stocks):
-			stock_2 = stocks[y]
-
-			item = {'stock_1' : stock_1, 'stock_2' : stock_2}
-			paired_list.append(item)
-
-	print "Paired list length: ", len(paired_list)
-	return paired_list
+# from src import math_tools
+from src.cointegrations_data import get_paired_stock_list, get_corrected_data, trim_data, propagate_on_fly
 
 
 def run_cointegrations():
+	"""
+	Takes in data from stocks, and performs a basic ADF test on the data.
+	"""
 
 	sectors = ('basic_materials', 'conglomerates', 'consumer_goods', 'financial', 'healthcare', 'industrial_services', \
 			   'services', 'technology', 'utilities')
@@ -156,7 +30,7 @@ def run_cointegrations():
 		stock_list[k] = new_val
 	in_file.close()
 
-	stock_list = ['XOM', 'CVX']
+	stock_list = ['GS', 'JPM']
 
 	paired_list = get_paired_stock_list(stock_list, fixed_stock=None)
 
@@ -174,35 +48,40 @@ def run_cointegrations():
 	time_start = time.time()
 
 	for k, item in enumerate(paired_list):
-		# stock_1_data = get_data(stock=item['stock_1'])
-		# stock_2_data = get_data(stock=item['stock_2'])
 		stock_1_data = manage_redis.parse_fast_data(item['stock_1'])
 		stock_2_data = manage_redis.parse_fast_data(item['stock_2'])
 		stock_1_close, stock_2_close, stock_1_trimmed, stock_2_trimmed = get_corrected_data(stock_1_data, stock_2_data)
 
-
-		end_data = 10969
 		end_data = len(stock_1_close)
 		window_size = 400
 		for x in xrange(0, end_data - window_size):
+		# for x in xrange(0, 12000):
 			end_index = x + window_size
 				
 			stock_1_window = stock_1_close[x : end_index+1]
 			stock_2_window = stock_2_close[x : end_index+1]
 
 			slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(stock_1_window, stock_2_window)
+			lin_reg_results = (slope, intercept, r_value, p_value, std_err)
 
-			if r_value >= 0.90 and x >= 6000:
-				result = show_residuals(stock_1_close, stock_2_close, x, end_index, stock_1_trimmed, stock_2_trimmed)
-				print "\nIndex of price corr: ", x, end_index
-				
+			# Here, we will only consider stocks where we can first see that a linreg of their prices can be done with
+			# a high r-value. If we can't even find a good correlation between the stocks' prices, we skip. This is like
+			# a sanity check - we should not be trying to trade 'MSFT' and 'XOM' as a pair trade!!!
+			if r_value >= 0.90:
+				result = create_synthetic_investment(stock_1_close, stock_2_close, x, end_index, stock_1_trimmed, \
+													 stock_2_trimmed, lin_reg_results)
+				# print "Window of high correlation used for prev. ADF Test: ", x, end_index
 				if result is not None:
-					print "\nFound a good trade: "
+					print "\nFound a good window: "
 					print "Start: ", stock_1_trimmed[x]['Date']
 					print "End: ", stock_2_trimmed[end_index]['Date']
 					out_file.write(result)
 					out_file.close
 					return
+					pass
+			else:
+				# print "PRICES NOT CORRELATED", x
+				pass
 
 	out_file.close()
 	print "\nFinished: ", len_pairs
@@ -214,238 +93,116 @@ def run_cointegrations():
 
 
 
-def show_residuals(stock_1_close, stock_2_close, start_index, end_index, stock_1_trimmed, stock_2_trimmed):
+def create_synthetic_investment(stock_1_close, stock_2_close, start_index, end_index, stock_1_trimmed, \
+							    stock_2_trimmed, lin_reg_results):
+	"""
+	This uses the hedge ratio (slope) of the regression to create a synthetic investment and create a list of all the
+	residuals of that sythetic investment based on the hedge ratio. If the hedge ratio were perfect, the synthetic
+	investment would always have a value of zero, since differencing is done. However, to trade it we look for a
+	synthetic investment that has residual values that fluctuate around zero with some level of predictability. We use
+	the Dickey Fuller test to check stationarity of the synthetic investment.
+
+	Performs an ADF test using a particular window of data for the stocks, given that we already know the prices are
+	highly correlated within the window of interest. The "trimmed" data here makes it much easier to print out the
+	dates of the data for debugging purposes.
+	"""
 			
 	stock_1_window = stock_1_close[start_index : end_index]
 	stock_2_window = stock_2_close[start_index : end_index]
-	
 	len_stock_1_window = len(stock_1_window)
 
-	slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(stock_1_window, stock_2_window)
+	slope, intercept, r_value, p_value, std_err = lin_reg_results
 
 	### Remember for the regressions: if we have this: first = [0,1,2,3,4] and second = [0,2,4,6,8] and we do:
 	# linregress(first, second), we get an equation of second = slope*first + intercept
 	
-	residuals = []
-	total_residuals = []
-	smoothed_total_residuals = []
-	current_residual = 0
-	total_residual = 0
-	spread_list = []
+	synthetic_price = 0
+	synthetic_prices = []
+	synthetic_price_pc = 0
+	synthetic_prices_pc = []
+	
 	output_string = ''
 
-	price_ratio_1_2 = stock_1_close[start_index] / stock_2_close[start_index]
 	hedge_ratio = slope
 
-	print "Start: ", stock_1_close[start_index], stock_2_close[start_index], price_ratio_1_2, hedge_ratio
-	print "\n\n"
-	#raise Exception
+	# print "Start: ", stock_1_close[start_index], stock_2_close[start_index], price_ratio_1_2, hedge_ratio
+	# print "\n\n"
 	previous_position_value = 0
 	for x in xrange(start_index, end_index):
-		
-		# placeholder in output string
-		stock_2_expected = 0
 
-		current_position_value = (hedge_ratio * stock_1_close[x]) - stock_2_close[x]
-		previous_position_value = (hedge_ratio * stock_1_close[x-1]) - stock_2_close[x-1]
-		current_residual = current_position_value - previous_position_value
-		residuals.append(current_residual)
-		total_residual += current_residual
-		total_residuals.append(total_residual)
+		# adding intercept simply corrects for an offset that would otherwise exist when the data is output
+		synthetic_price = (hedge_ratio * stock_1_close[x]) - stock_2_close[x] + intercept
+		synthetic_prices.append(synthetic_price)
 
-
-		if len(total_residuals) > 7:
-			z = x - start_index
-			smoothed_total_residual = np.mean(total_residuals[z-6:z+1])
-			smoothed_total_residuals.append(smoothed_total_residual)
-		else:
-			smoothed_total_residual = 0
-			smoothed_total_residuals.append(smoothed_total_residual)
-
+		synthetic_price_pc = synthetic_price / ((hedge_ratio * stock_1_close[start_index]) + stock_2_close[start_index] + intercept)
+		synthetic_prices_pc.append(synthetic_price_pc)
 
 		date = stock_1_trimmed[x]['Date']
-		print x, date, slope, intercept, stock_1_close[x], stock_2_close[x], current_residual, total_residual
+		### print x, date, slope, intercept, stock_1_close[x], stock_2_close[x], current_residual, total_residual
 
 		output_string += ','.join([
 								   # str(spread),
 								   str(stock_1_close[x]), 
 								   str(stock_2_close[x]),
-								   str(stock_2_expected),
-								   str(current_residual),
-								   str(total_residual),
-								   # str(smoothed_total_residual),
+								   str(0),
+								   str(synthetic_price_pc),
+								   str(0),
+								   # str(smoothed_total_residual)
 								   ]) + '\n'
+		## print x, date, round(total_residual, 4), round(total_residual_pc, 6), '%'
 
 
-	rslope, rintercept, rr_value, rp_value, rstd_err = scipy.stats.linregress(range(len_stock_1_window), total_residuals)
+	rslope, rintercept, rr_value, rp_value, rstd_err = scipy.stats.linregress(range(len_stock_1_window), \
+																					synthetic_prices_pc)
 	
-	print '\nRes slope, int, p, r: %.4f, %.4f, %.4f, %.4f' % (rslope, rintercept, rp_value, rr_value)
-	print "r, slope, intercept: ", r_value, slope, intercept
-
-	# if abs(rr_value) <= 0.3 and abs(rslope) < 0.001:
-	if abs(rr_value) <= 0.003 and abs(rp_value) >= 0.95:
-
-		import statsmodels.tsa.stattools as stats
-		# see here: http://statsmodels.sourceforge.net/stable/generated/statsmodels.tsa.stattools.adfuller.html#statsmodels.tsa.stattools.adfuller
-		result = stats.adfuller(residuals)
-		print result, '\n\n\n'
-		raise Exception
-
-		# we need to add to the residual list here to continue the simulation
-		###################
-		print "\n"
-		for y in xrange(0, 500):
-
-			i = y + end_index
-
-			stock_2_expected = 0
-
-			current_position_value = (hedge_ratio * stock_1_close[i]) - stock_2_close[i]
-			previous_position_value = (hedge_ratio * stock_1_close[i-1]) - stock_2_close[i-1]
-			current_residual = current_position_value - previous_position_value
-			residuals.append(current_residual)
-			total_residual += current_residual
-			total_residuals.append(total_residual)
-
-
-			z = len_stock_1_window + y
-			smoothed_total_residual = np.mean(total_residuals[z-6:z+1])
-			smoothed_total_residuals.append(smoothed_total_residual)
-
-
-			date = stock_1_trimmed[i]['Date']
-			print i, date, slope, intercept, stock_1_close[i], stock_2_close[i], current_residual, total_residual
-
-			output_string += ','.join([
-									   # str(spread),
-									   str(stock_1_close[i]), 
-									   str(stock_2_close[i]),
-									   str(stock_2_expected),
-									   str(current_residual),
-									   str(total_residual),
-									   # str(smoothed_total_residual)
-									   ]) + '\n'
-
-
-
-
-		print "\nMean / Sigma of total resid: ", np.mean(total_residuals), np.std(total_residuals)
-		####################
-		return output_string
+	# print '\nTotal residuals slope, int, p, r: %.4f, %.4f, %.4f, %.4f' % (rslope, rintercept, rp_value, rr_value)
+	# print 'We should expect slope near zero, low r value wrt time and low p value to indicate it is not random chance'
+	# print '\nStock prices r, slope, intercept: %.4f, %.4f, %.4f' % (r_value, slope, intercept)
+	
+	# We first run a sanity check before even attempting a DF test - here, we verify that the r value of the synthetic
+	# investment is somewhat low - it should be, indicating that it is not particularly correlated to the time. 
+	if abs(rr_value) <= 0.5:
+		# print 'Synth inv: %i %.4f %.4f %.4f %.4f %.4f' % (start_index, rslope, rintercept, rr_value, rp_value, rstd_err)
+		result = do_df_test(synthetic_prices_pc)
 	else:
+		# print 'SKIPPED: ', start_index
 		return None
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def show_residuals(stock_1_close, stock_2_close, start_index, end_index):
-			
-# 	stock_1_window = stock_1_close[start_index : end_index]
-# 	stock_2_window = stock_2_close[start_index : end_index]
-	
-# 	len_stock_1_window = len(stock_1_window)
-
-# 	# print "Len: ", len_stock_1_window, (end_index - start_index), start_index, end_index
-# 	# print stock_1_window
-# 	# print stock_2_window
-
-# 	slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(stock_1_window, stock_2_window)
-
-# 	### Remember for the regressions: if we have this: first = [0,1,2,3,4] and second = [0,2,4,6,8] and we do:
-# 	# linregress(first, second), we get an equation of second = slope*first + intercept
-	
-# 	residuals = []
-# 	current_residual = 0
-# 	total_residual = 0
-# 	spread_list = []
-# 	output_string = ''
-
-# 	for x in xrange(start_index, end_index):
+	if result and result[0] <= result[4]['5%'] and result[1] < 0.1:
+		print start_index, stock_1_trimmed[start_index]['Date'], result[0], result[4]['5%'], result[1], \
+			  round(slope, 4), round(intercept, 4)
 		
-# 		stock_2_expected = (slope * stock_1_close[x]) + intercept
-# 		current_residual = stock_2_expected - stock_2_close[x]
-# 		total_residual += current_residual
-# 		residuals.append(current_residual)
+		mean_price = round(np.mean(synthetic_prices_pc), 5)
+		sigma_price = round(np.std(synthetic_prices_pc), 5)
+		if sigma_price < 0.05:
+			return None
 
-# 		print x, slope, intercept, stock_1_close[x], stock_2_close[x], stock_2_expected, current_residual, total_residual
-
-# 		output_string += ','.join([
-# 								   # str(spread),
-# 								   str(stock_1_close[x]), 
-# 								   str(stock_2_close[x]),
-# 								   str(stock_2_expected),
-# 								   str(current_residual),
-# 								   str(total_residual),
-# 								   ]) + '\n'
-
-
-# 	rslope, rintercept, rr_value, rp_value, rstd_err = scipy.stats.linregress(range(len_stock_1_window), residuals)
+		print "Synth investment stats: ", mean_price, sigma_price, '\n'
+		return output_string
+	elif result:
+		# print "FAILED DF TEST: ", start_index, result[0], result[4]['5%'], result[1], round(slope, 4), round(intercept, 4)
+		pass
 	
-# 	print '\nRes slope, int, p, r: %.4f, %.4f, %.4f, %.4f' % (rslope, rintercept, rp_value, rr_value)
-# 	print "r, slope, intercept: ", r_value, slope, intercept
+	return None
 
-# 	# if abs(rr_value) <= 0.3 and abs(rslope) < 0.001:
-# 	if abs(rr_value) <= 0.003 and abs(rp_value) >= 0.95:
 
-# 		# we need to add to the residual list here to continue the simulation
-# 		###################
-		
-# 		print "\n"
-# 		for y in xrange(0, 50):
 
-# 			i = y + end_index
+def do_df_test(prices):
 
-# 			stock_2_expected = (slope * stock_1_close[i]) + intercept
-# 			current_residual = stock_2_expected - stock_2_close[i]
-# 			total_residual += current_residual
-# 			residuals.append(current_residual)
-
-# 			print i, slope, intercept, stock_1_close[i], stock_2_close[i], stock_2_expected, current_residual, total_residual
-
-# 			output_string += ','.join([
-# 									# str(spread),
-# 									str(stock_1_close[i]), 
-# 									str(stock_2_close[i]),
-# 									str(stock_2_expected),
-# 									str(current_residual),
-# 									str(total_residual),
-# 									]) + '\n'
+	import statsmodels.tsa.stattools as stats
+	# see here: http://statsmodels.sourceforge.net/stable/generated/statsmodels.tsa.stattools.adfuller.html#statsmodels.tsa.stattools.adfuller
+	# result is in the format: test statistic (lambda), p_value, used lag, # of observations, critical values
+	result = stats.adfuller(prices)
+	return result
 
 
 
 
-# 		####################
-# 		return output_string
-# 	else:
-# 		return None
-
-	# print "\nCorr Coeff: ", np.corrcoef(stock_1_window, stock_2_window)[0][1]
-
-	# print "\n\nResidual Slope: ", rslope
-	# print "Residual Intercept: ", rintercept
-	# print "Residual R Value: ", rr_value
-	# print "Residual P Value: ", rp_value
-	# print "Residual Std Err: ", rstd_err
-
-	# print "\n\nSlope: ", slope
-	# print "Intercept: ", intercept
-	# print "R Value: ", r_value
-	# print "P Value: ", p_value
-	# print "Std Err: ", std_err
+### I had an idea to use smoothing on the residuals but I don't think this makes sense...
+# if len(total_residuals) > 7:
+# 	z = x - start_index
+# 	smoothed_total_residual = np.mean(total_residuals[z-6:z+1])
+# 	smoothed_total_residuals.append(smoothed_total_residual)
+# else:
+# 	smoothed_total_residual = 0
+# 	smoothed_total_residuals.append(smoothed_total_residual)
---
+++ src/cointegrations_data.py
@@ -0,0 +1,151 @@
+import datetime
+import copy
+import numpy as np
+
+from src import math_tools
+
+
+def get_corrected_data(stock_1_data, stock_2_data, get_returns = None):
+	"""
+	Performs the "trimming function" on stocks (see below). Returns "simple" returns after stocks are trimmed, Using
+	the numpy array format. Also returns the full trimmed dictionary. Also calculates the ln returns of the simple
+	returns (since it is convenient to do here).
+	"""
+	stock_1_trimmed, stock_2_trimmed = trim_data(stock_1_data, stock_2_data)
+
+	simple_1_close = np.empty(len(stock_1_trimmed))
+	simple_2_close = np.empty(len(stock_2_trimmed))
+
+	for k, v in enumerate(stock_1_trimmed):
+		simple_1_close[k] = stock_1_trimmed[k]['AdjClose']
+		simple_2_close[k] = stock_2_trimmed[k]['AdjClose']
+
+	if get_returns:
+		simple_1_returns = math_tools.get_ln_returns(simple_1_close)
+		simple_2_returns = math_tools.get_ln_returns(simple_2_close)
+
+	return simple_1_close, simple_2_close, stock_1_trimmed, stock_2_trimmed
+	# return simple_1_returns, simple_2_returns
+
+def trim_data(stock_1_data, stock_2_data):
+	"""
+	This "trims" data on two stocks so that the two data sets will have the same start date and the same end date. It
+	simply removes excess data from the stock with an earlier start date. It then checks that the length of the two
+	sets of data. It tries to "propagate" the data to make the lengths match. If this fails, it raises an exception. 
+	Then, it not only checks lengths, but also checks that the actual dates used match each other. If one date is
+	missing, it raises an exception.
+	"""
+
+	stock_1_start = datetime.datetime.strptime(stock_1_data[0]['Date'], '%Y-%m-%d').date()
+	stock_2_start = datetime.datetime.strptime(stock_2_data[0]['Date'], '%Y-%m-%d').date()
+
+	if stock_1_start < stock_2_start:
+		for x in xrange(0, len(stock_1_data)):
+			# if datetime.datetime.strptime(stock_2_data[x]['Date'], '%Y-%m-%d').date() >= \
+			#    datetime.datetime.strptime(stock_1_data[0]['Date'], '%Y-%m-%d').date():
+			if stock_1_data[x]['Date'] == stock_2_data[0]['Date']:
+				trim_at = x
+				break
+		stock_1_data = stock_1_data[trim_at:]
+	
+	elif stock_2_start < stock_1_start:
+		for x in xrange(0, len(stock_2_data)):
+			# if datetime.datetime.strptime(stock_2_data[x]['Date'], '%Y-%m-%d').date() >= \
+			#    datetime.datetime.strptime(stock_1_data[0]['Date'], '%Y-%m-%d').date():
+			if stock_2_data[x]['Date'] == stock_1_data[0]['Date']:
+				trim_at = x
+				break
+		stock_2_data = stock_2_data[trim_at:]
+
+	if len(stock_1_data) != len(stock_2_data):
+		stock_1_data, stock_2_data = propagate_on_fly(stock_1_data, stock_2_data)
+
+	if len(stock_1_data) != len(stock_2_data) or \
+		stock_2_data[len(stock_2_data)-1]['Date'] != stock_1_data[len(stock_1_data)-1]['Date'] or \
+		stock_2_data[0]['Date'] != stock_1_data[0]['Date']:
+		# print "\n**************"
+		# print stock_1_data[0]['Symbol'], stock_2_data[0]['Symbol']
+		# print len(stock_1_data), len(stock_2_data)
+		# print stock_1_data[0]['Date'], stock_2_data[0]['Date']
+		# print stock_1_data[len(stock_1_data)-1]['Date'], stock_2_data[len(stock_2_data)-1]['Date']
+		# for x in xrange(len(stock_1_data)):
+		# 		if stock_1_data[x]['Date'] != stock_2_data[x]['Date']:
+		# 			print stock_1_data[x]['Date'], stock_2_data[x]['Date']
+		# 			print "**************"
+		# 			break
+		e = stock_1_data[0]['Symbol'] + ' and ' + stock_2_data[0]['Symbol']
+		raise Exception(e + ' did not trim properly and cannot be processed')
+
+	# TODO: this section should be cythonized
+	for x in xrange(len(stock_1_data)):
+		if stock_1_data[x]['Date'] != stock_2_data[x]['Date']:
+			e = stock_1_data[0]['Symbol'] + ' and ' + stock_2_data[0]['Symbol']
+			print "\n\n"
+			raise Exception(e + ' did not trim properly and cannot be processed')
+
+	return stock_1_data, stock_2_data
+
+def propagate_on_fly(stock_1_data, stock_2_data):
+	"""
+	Searches through date data and compares dates between the two series. If it finds that one series skips a date that
+	the other series had, it simply propagates the previous day's data to the missing date and uses the date the was
+	found in the more complete data series. 
+	"""
+
+	x_max = max(len(stock_1_data), len(stock_2_data))
+	
+	for x in xrange(0, x_max):
+		
+		# if x > 3397 and x < 3407:
+		# 	print x, stock_1_data[x]['Date'], stock_2_data[x]['Date'], stock_1_data[x]['Close'], stock_2_data[x]['Close']
+		if stock_1_data[x]['Date'] != stock_2_data[x]['Date']:
+			if datetime.datetime.strptime(stock_1_data[x]['Date'], '%Y-%m-%d').date() > \
+				datetime.datetime.strptime(stock_2_data[x]['Date'], '%Y-%m-%d').date():
+				temp = copy.deepcopy(stock_1_data[x-1])
+				temp['Date'] = copy.deepcopy(stock_2_data[x]['Date'])
+				stock_1_data.insert(x, temp)
+
+			elif datetime.datetime.strptime(stock_2_data[x]['Date'], '%Y-%m-%d').date() > \
+				datetime.datetime.strptime(stock_1_data[x]['Date'], '%Y-%m-%d').date():
+				temp = copy.deepcopy(stock_2_data[x-1])
+				temp['Date'] = copy.deepcopy(stock_1_data[x]['Date'])
+				stock_2_data.insert(x, temp)
+
+	# for x in xrange(3397, 3408):
+	# 	print x, stock_1_data[x]['Date'], stock_2_data[x]['Date'], stock_1_data[x]['Close'], stock_2_data[x]['Close']
+		# if stock_1_data[x]['Date'] != stock_2_data[x]['Date']:
+		# 	# print stock_1_data[x]['Date'], stock_2_data[x]['Date']
+		# 	break
+
+	return stock_1_data, stock_2_data
+
+
+
+def get_paired_stock_list(stocks, fixed_stock=None):
+	"""
+	Generates a list of dictionaries. Each dictionary contains 2 stocks - a pair. Using the fixed stock option will
+	basically just create a pairing of all stocks with that one stock. Otherwise, it creates every pair possible.
+	"""
+
+	len_stocks = len(stocks)
+	paired_list = []
+	if fixed_stock is not None:
+		for x in xrange(0, len_stocks):
+			item = {'stock_1' : fixed_stock, 'stock_2' : stocks[x], 'corr_coeff' : 0.0, 'beta' : 0.0}
+			paired_list.append(item)
+		print "Paired list length: ", len(paired_list)
+		return paired_list
+
+	for x in xrange(0, len_stocks):
+		stock_1 = stocks[x]
+
+		for y in xrange(x+1, len_stocks):
+			stock_2 = stocks[y]
+
+			item = {'stock_1' : stock_1, 'stock_2' : stock_2, 'corr_coeff' : 0.0, 'beta' : 0.0}
+			paired_list.append(item)
+
+	print "Paired list length: ", len(paired_list)
+	return paired_list
+
+
