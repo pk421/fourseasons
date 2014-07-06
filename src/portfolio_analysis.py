2eab8735a5946e7c263f498e5dc8aeb76ff5a6e8
Michael Pilat <mcpilat@gmail.com>
2014-07-06 13:01:24 -0400

    WIP port analysis

--- src/portfolio_analysis.py
+++ src/portfolio_analysis.py
@@ -20,7 +20,7 @@ def run_portfolio_analysis():
     assets_list = ['SPY', 'EFA', 'EWJ', 'EEM', 'IYR', 'RWX', 'IEF', 'TLT', 'DBC', 'GLD']
     # assets_list = ['SPY', 'SHY', 'GLD']
     # assets_list = ['TNA', 'EFA', 'EWJ', 'EEM', 'VNQ', 'RWX', 'IEF', 'TLT', 'DBC', 'SLV']
-    # assets_list = ['SPY', 'GLD']
+   # assets_list = ['SPY', 'GLD']
 
     mdp_port = MDPPortfolio(assets_list=assets_list)
 
@@ -34,26 +34,70 @@ def run_portfolio_analysis():
         logging.warning("Get Data returned False, Failure")
         return False
 
-#    rebalance_time = 90
-#    port.weights = [ [1.0 / len(port.assets)] for x in port.assets]
-#
-#    while x < len(mdp_port.trimmed[mdp_port.assets[0]]):
-#
-#        if not (x == 0 or x % rebalance_time == 0):
-#            x = x-1
-#            continue
-#
-#
-#
-#
-#
-#
-#
-#        x = x-1
+    rebalance_time = 90
+    mdp_port.weights = [ [1.0 / len(mdp_port.assets)] for x in mdp_port.assets]
+    mdp_port.normalized_weights = mdp_port.weights
 
-    covariance_matrix = mdp_port.get_covariance_matrix(x=len(mdp_port.trimmed[mdp_port.assets[0]]), lookback=50)
+
+    current_portfolio_valuation = get_port_valuation(mdp_port, x=0)
+
+    x=0
+    previous_rebalance = 1
+    while x < len(mdp_port.trimmed[mdp_port.assets[0]]):
+#        if x == 0:
+#            x += 1
+#            continue
+        if not (x % rebalance_time == 0) or x == 0:
+            current_portfolio_valuation = get_port_valuation(mdp_port, x=x)
+            mdp_port.portfolio_valuations.append((mdp_port.trimmed[mdp_port.assets[0]][x]['Date'], current_portfolio_valuation / previous_rebalance))
+            print x, mdp_port.trimmed[mdp_port.assets[0]][x]['Date'], current_portfolio_valuation / previous_rebalance
+            x += 1
+            continue
+        else:
+            # When calculating the rebalance ratio, we first assume we used the old weights for today's valuation. Then
+            # we calculate new weights for today, value the portfolio for today, then find the ratio for today if we
+            # had used the old weights for today
+
+            old_weighted_valuation = get_port_valuation(mdp_port, x=x) / previous_rebalance
+            print "old weighted: ", old_weighted_valuation
+
+
+            mdp_port.rebalance(x, lookback=30)
+            current_portfolio_valuation = get_port_valuation(mdp_port, x=x)
+
+            previous_rebalance = current_portfolio_valuation / old_weighted_valuation
+            print "Previous Rebalance: ", previous_rebalance
+
+            mdp_port.portfolio_valuations.append((mdp_port.trimmed[mdp_port.assets[0]][x]['Date'], current_portfolio_valuation / previous_rebalance))
+            print x, mdp_port.trimmed[mdp_port.assets[0]][x]['Date'], current_portfolio_valuation / previous_rebalance
+            x+=1
+
+    print "len: ", len(mdp_port.trimmed[mdp_port.assets[0]]), len(mdp_port.portfolio_valuations)
+
+    print mdp_port.trimmed[mdp_port.assets[0]][0], mdp_port.trimmed[mdp_port.assets[0]][-1]
+    print '\n', mdp_port.portfolio_valuations[0], mdp_port.portfolio_valuations[-1]
+
+
+#    normalized_weights = mdp_port.get_covariance_matrix(x=len(mdp_port.trimmed[mdp_port.assets[0]]), lookback=50)
 
     return
+
+def get_port_valuation(port, x=0):
+
+    ### TODO: accept a parameter for rebalance ratio so that it is not divided everywhere else
+
+    weights = port.normalized_weights
+    total_valuation = 0
+
+    for k, v in enumerate(port.assets):
+        total_valuation += weights[k][0] * port.closes[v][x]
+        # print k, v, weights[k], port.closes[v][x], weights[k][0] * port.closes[v][x]
+
+    logging.debug('%s %s %s' % (x, port.trimmed[port.assets[0]][x]['Date'], total_valuation))
+
+    return total_valuation
+
+
 
 
 
@@ -152,16 +196,21 @@ class MDPPortfolio():
 
         self.assets = assets_list
         self.weights = []
+        self.normalized_weights = []
+        self.portfolio_returns = []
+        self.portfolio_valuations = []
 
         # These should not get reset
         self.closes = {}
         self.trimmed = {}
 
-        self.returns = {}
+        self.forward_returns = {}
+
+        self.past_returns = {}
         self.volatilities = {}
 
         #Matrices / vectors
-        self.returns_matrix = None
+        self.past_returns_matrix = None
         self.volatilities_matrix = None
         self.cov_matrix = None
         self.inv_cov_matrix = None
@@ -173,7 +222,7 @@ class MDPPortfolio():
         for item in assets_list:
             self.closes[item] = []
             self.trimmed[item] = []
-            self.returns[item] = []
+            self.past_returns[item] = []
             self.volatilities[item] = []
 
     def validate_portfolio(self):
@@ -202,6 +251,16 @@ class MDPPortfolio():
 
         return True
 
+    def rebalance(self, x, lookback=30):
+
+        current_weights = self.normalized_weights
+        current_valuation = get_port_valuation(self, x)
+        new_weights = self.get_covariance_matrix(x, lookback=lookback)
+
+
+
+        return True
+
     def get_covariance_matrix(self, x, lookback=0):
 
         end_index = x
@@ -213,20 +272,21 @@ class MDPPortfolio():
 
         for item in self.assets:
             closes = self.closes[item][start_index:end_index]
-            self.returns[item] = get_returns(closes)
+            self.past_returns[item] = get_returns(closes)
 
         for z in self.assets:
-            rets = (self.returns[z])
+            rets = (self.past_returns[z])
             self.volatilities[z] = np.std(rets)
 
         # using (lookback-1) cuts out a leading zero that appears on day 1, since there is no return yet
-        self.returns_matrix = np.array([self.returns[item] for item in self.assets])
+        self.past_returns_matrix = np.array([self.past_returns[item] for item in self.assets])
     #    print "\nRETURNS: \n", self.returns_matrix
 
 
         volatilities = [ [self.volatilities[z]] for z in self.assets ]
         self.volatilities_matrix = np.array(volatilities)
-        self.cov_matrix = np.cov(self.returns_matrix)
+        self.cov_matrix = np.cov(self.past_returns_matrix)
+
         self.inv_cov_matrix = scipy.linalg.inv(self.cov_matrix)
         self.transposed_volatilities_matrix = np.matrix.transpose(self.volatilities_matrix)
 
@@ -240,19 +300,20 @@ class MDPPortfolio():
         numerator = np.dot(self.inv_cov_matrix, self.volatilities_matrix)
         denominator_a = np.dot(self.transposed_volatilities_matrix, self.inv_cov_matrix)
         denominator = np.dot(denominator_a, self.volatilities_matrix)
-        weights = np.divide(numerator, denominator)
+        self.weights = np.divide(numerator, denominator)
 
-        print numerator, '\n\n', denominator_a, '\n\n', denominator, '\n\n', weights
+        # print numerator, '\n\n', denominator_a, '\n\n', denominator, '\n\n', self.weights
 
-        total_sum = sum(weights)[0]
+        total_sum = sum(self.weights)[0]
 
-        print "\nSUM: ", total_sum
+        # print "\nSUM: ", total_sum
 
-        normalized_weights = np.divide(weights, total_sum)
+        self.normalized_weights = np.divide(self.weights, total_sum)
 
-        print "\nNormalized Weights:\n", normalized_weights
+        print "\nNormalized Weights:\n", self.normalized_weights
+        print "\nSum of normalized: \n", sum(self.normalized_weights)[0]
 
-        return normalized_weights
+        return self.normalized_weights
 
 
     #    port_constraints = [{'type': 'eq', 'fun': positive_sum_only},\
